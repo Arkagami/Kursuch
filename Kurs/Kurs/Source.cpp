3ff3a3c25fabe5f22953444d634a2dc3a1b70301
Arkagami <mixno20@gmail.com>
2017-04-17 02:46:42 +0300

    Херня какая-то

---
+++ Kurs/Kurs/Source.cpp
@@ -0,0 +1,9 @@
+#include <windows.h>
+#include <tchar.h>
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+	MessageBox(NULL, _T("Ты пидр! Соси!!!"),
+		_T("Программа"), MB_OK);
+	return 0;
+}
+//http://rutracker.org/forum/viewtopic.php?t=3401053
